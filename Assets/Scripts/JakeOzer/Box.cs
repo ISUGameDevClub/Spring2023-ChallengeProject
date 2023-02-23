02713f2a0c7970d5d962714b8c152b31d5ab143d
Jake Ozer <jakobozer1@gmail.com>
2023-02-22 21:42:25 -0600

    Conveyor Prefab sprite updated
    Conveyor prefabs sprite is updated. Also added fields for boxFill and boxFillMax on Box object

--- Assets/Scripts/JakeOzer/Box.cs
+++ Assets/Scripts/JakeOzer/Box.cs
@@ -6,7 +6,9 @@ public class Box : MonoBehaviour
 {
     private float conveyorTime;
     private int conveyorIndex = 0;
-    
+    private float boxFill = 0;
+    private float boxFillMax = 100;
+
     private void Start()
     {
         conveyorTime = Time.time;
@@ -14,6 +16,7 @@ public class Box : MonoBehaviour
 
     public float GetConveyorTime() => conveyorTime;
     public int GetConveyorIndex() => conveyorIndex;
+    public float GetBoxFill() => boxFill;
 
     public void NextConveyor()
     {
