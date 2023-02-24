c3404f9fe3530f835a9a1deae308477a57635c88
PaulSeitzISU <97689370+PaulSeitzISU@users.noreply.github.com>
2023-02-24 10:59:27 -0600

    better Paint

--- Assets/Scripts/conveyor/LastGameObjectChecker.cs
+++ Assets/Scripts/conveyor/LastGameObjectChecker.cs
@@ -6,9 +6,12 @@ public class LastGameObjectChecker : MonoBehaviour
     public bool canBeDestroyed;
 
     public List<GameObject> gameObjectsList; // The list of game objects to check
+    public bool isFristGameObject; // A flag to indicate whether this game object is the last one in the list
     public bool isLastGameObject; // A flag to indicate whether this game object is the last one in the list
     public GameObject boxMover;
-    public int minorListNumber;
+
+    public List<GameObject> minorList;
+    public int minorListNumber = -1;
 
     void Start()
     {
@@ -18,23 +21,56 @@ public class LastGameObjectChecker : MonoBehaviour
 
     private void Update()
     {
+
+
         // Get a reference to the game object's index in the list
-        int index = gameObjectsList.IndexOf(gameObject);
 
         // Check if the game object is the last one in the list
-        if (index == -1)
-        {
 
-        }
-        else if (index == gameObjectsList.Count - 1)
+        if (minorListNumber != -1)
         {
-            isLastGameObject = true;
-            GetComponent<SpriteRenderer>().color = new Color(.9f, 0.4f, 1f, 1f);
+            int index = minorList.IndexOf(gameObject);
+            if (index == -1)
+            {
+
+            }
+            else if (index == 0)
+            {
+                isLastGameObject = true;
+                GetComponent<SpriteRenderer>().color = new Color(0f, 1f, 0f, 1f);
+            }
+            else if (index == minorList.Count - 1)
+            {
+                isLastGameObject = true;
+                GetComponent<SpriteRenderer>().color = new Color(.9f, 0.4f, 1f, 1f);
+            }
+            else
+            {
+                isLastGameObject = false;
+                GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f);
+            }
+
         }
         else
         {
-            isLastGameObject = false;
-            GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f);
+            int index = gameObjectsList.IndexOf(gameObject);
+
+            if (index == -1)
+            {
+
+            }
+            else if (index == gameObjectsList.Count - 1)
+            {
+                isLastGameObject = true;
+                GetComponent<SpriteRenderer>().color = new Color(.9f, 0.4f, 1f, 1f);
+            }
+            else
+            {
+                isLastGameObject = false;
+                GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f);
+            }
         }
+
+
     }
 }
