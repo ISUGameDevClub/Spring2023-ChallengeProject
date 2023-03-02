380706a5fe521bce739729f139f741cf08f5715c
PaulSeitzISU <97689370+PaulSeitzISU@users.noreply.github.com>
2023-03-01 20:21:04 -0600

    small fix

--- Assets/Scripts/Camera/TileMapCameraController.cs
+++ Assets/Scripts/Camera/TileMapCameraController.cs
@@ -7,21 +7,17 @@ public class TileMapCameraController : MonoBehaviour
     public int startingTileY; // the y-coordinate of the starting tile
     public float offsetX; // the offset from the center of the starting tile
     public float offsetY; // the offset from the center of the starting tile
+    public float boarderOffset; // increases offset of boarder measered in cells
     public int numCellsVertical; // the number of grid cells to display vertically
+
+    private void Start()
+    {
+        SetCamera();
+    }
 
     private void Update()
     {
-        // get the size of a single tile
-        Vector3 cellSize = grid.cellSize;
-
-        // get the position of the starting tile
-        Vector3 startingTilePos = grid.GetCellCenterWorld(new Vector3Int(startingTileX, startingTileY, 0));
-
-        // set the camera's position to the center of the starting tile, plus the specified offset
-        transform.position = startingTilePos + new Vector3(offsetX, offsetY, -10);
-
-        // set the camera's orthographic size to show the specified number of grid cells vertically
-        Camera.main.orthographicSize = numCellsVertical * cellSize.y / 2;
+        SetCamera();
     }
 
     public void SetCamera()
@@ -36,7 +32,7 @@ public class TileMapCameraController : MonoBehaviour
         transform.position = startingTilePos + new Vector3(offsetX, offsetY, -10);
 
         // set the camera's orthographic size to show the specified number of grid cells vertically
-        Camera.main.orthographicSize = numCellsVertical * cellSize.y / 2;
+        Camera.main.orthographicSize = (numCellsVertical * cellSize.y / 2) + (boarderOffset * cellSize.y / 2);
     }
 
 }
