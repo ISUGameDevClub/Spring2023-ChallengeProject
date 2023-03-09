3d0c298a3c2b728116b46dbb0424da9a2440ac32
nemodev42 <nemodev42@gmail.com>
2023-03-09 13:09:33 -0600

    Audio Track Scripts

---
+++ Assets/Scripts/Audio/AudioTrack.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AudioTrack
+{
+    [SerializeField]
+    private AudioClip clip;
+    [SerializeField]
+    private float maxVolume;
+
+    private AudioSource source;
+
+    // public AudioClip Clip => clip;
+    // public AudioSource Source => source;
+    public float MaxVolume => maxVolume;
+
+    public void Initialize( GameObject hostObject ) {
+        if ( source != null) {
+            Debug.LogError("AudioTrack already initialized");
+            return;
+        }
+
+        source = hostObject.AddComponent<AudioSource>();
+        source.hideFlags = HideFlags.HideInInspector;
+        source.clip = clip;
+        source.volume = 0f;
+        source.loop = true;
+
+        source.Play();
+    }
+
+    public IEnumerator FadeTrack( float targetVolume, float duration ) {
+        float startVolume = source.volume;
+        float time = 0f;
+
+        while ( time < duration ) {
+            time += Time.deltaTime;
+            float volume = Mathf.Lerp(startVolume, targetVolume, time / duration);
+            source.volume = volume;
+            yield return null;
+        }
+
+        source.volume = targetVolume;
+    }
+}
