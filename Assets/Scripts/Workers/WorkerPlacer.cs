2c89b61a54f6308bfb49bcbfb81135b7d1ea057f
Jake Ozer <jakobozer1@gmail.com>
2023-04-05 20:54:41 -0500

    Revert "ui implementation done"
    This reverts commit c36f3a74d4a25022030fb323b4197b93e58b5ce7.

--- Assets/Scripts/Workers/WorkerPlacer.cs
+++ Assets/Scripts/Workers/WorkerPlacer.cs
@@ -7,22 +7,15 @@ public class WorkerPlacer : MonoBehaviour
 {
     [SerializeField] private GameObject worker;
     [SerializeField] private GameObject ghostWorker;
-    private UIBottomPanelController bottomPanelController;
     private GameObject tempWorker;
     private bool isPlacing = false;
 
     Vector2 mousePos;
 
-    private void Start()
-    {
-        bottomPanelController = FindObjectOfType<UIBottomPanelController>();
-    }
-
     public void StartPlacing()
     {
         isPlacing = true;
         tempWorker = Instantiate(ghostWorker, mousePos, Quaternion.identity);
-        bottomPanelController.Toggle();
     }
 
     private void Update()
@@ -36,9 +29,8 @@ public class WorkerPlacer : MonoBehaviour
             Destroy(tempWorker);
             Instantiate(worker, mousePos, Quaternion.identity);
             isPlacing = false;
-            bottomPanelController.Toggle();
         }
     }
 }
 
-//maybe need to make it so when placing, the ui slider goes down
+//RAYCASTING NEEDED TO CHECK IF HITTING SOMETHING NOT INTENDED
